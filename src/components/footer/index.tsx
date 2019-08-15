9f1f4d5d20c42871f4147eba3a0aa3907eb5b74b
Tanya92 <tatiana_mironchik@tut.by>
2019-08-15 03:52:22 +0300

    refactor: change responsive view

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,19 +6,22 @@ export default class Footer extends React.Component<object> {
         return (
             <footer className="footer-container">
                 <p className="author-name">Author: Tatsiana Mironchyk</p>
-                <div className="icon-container">
-                    <a href="https://github.com/Tanya92" target="_blank" className="info-link">
-                        <img src="../assets/github-logo.svg" alt="github_icon" className="icon"></img>
-                    </a>
-                    <a href="https://t.me/tatsiana_mironchyk" target="_blank" className="info-link">
-                        <img src="../assets/telegram.svg" alt="telegram_icon" className="icon"></img>
-                    </a>
+                <div className="links-container">
+                    <div className="icons-container">
+                        <a href="https://github.com/Tanya92" target="_blank" className="info-link">
+                            <img src="../assets/github-logo.svg" alt="github_icon" className="icon"/>
+                        </a>
+                        <a href="https://t.me/tatsiana_mironchyk" target="_blank" className="info-link">
+                            <img src="../assets/telegram.svg" alt="telegram_icon" className="icon"/>
+                        </a>
+                        <a href="mailto:tatiana_mironchik@tut.by" target="_blank" className="info-link">
+                            <img src="../assets/mail_logo.svg" alt="mail_icon" className="icon"/>
+                        </a>
+                    </div>
                     <a href="mailto:tatiana_mironchik@tut.by" target="_blank" className="info-link email-link">
-                        <img src="../assets/mail_logo.svg" alt="mail_icon" className="icon"></img>
                         <span className="mail-info">(tatiana_mironchik@tut.by)</span>
                     </a>
                 </div>
-
             </footer>
         );
     }
